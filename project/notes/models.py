ad489efb6894a4f2cf413d81446fc7e04533b367
DM <xvmodvx@gmail.com>
2018-11-04 10:59:52 -0800

    Notes Updated

--- project/notes/models.py
+++ project/notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="New")
+    text = models.TextField(default="Text")
+    notes = models.TextField(default="Notes")
+    pub_date = models.DateTimeField(auto_now_add=True, auto_now=False)
